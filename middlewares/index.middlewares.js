0af456f108f9c5e41445305266dc222e6aa50fa2
Emmanuel <emmanuelugwueze6@gmail.com>
2022-11-28 07:12:07 +0100

    index rputes

--- middlewares/index.middlewares.js
+++ middlewares/index.middlewares.js
@@ -1,8 +1,10 @@
 import express from 'express';
+import indexRoute from '../routes/index.routes.js';
 
 const middleware = (app) => {
     app.use(express.json());
-    app.use(express.urlencoded({extended:false}))
+    app.use(express.urlencoded({ extended: false }))
+    app.use(indexRoute);
 }
 
 export default middleware
