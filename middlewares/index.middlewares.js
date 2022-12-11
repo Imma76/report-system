18738a7966e992ae990e7d5cc827aae08fdaa4d4
Emmanuel <emmanuelugwueze6@gmail.com>
2022-12-11 23:37:37 +0100

    cors changes

--- middlewares/index.middlewares.js
+++ middlewares/index.middlewares.js
@@ -2,11 +2,12 @@ import express from 'express';
 import indexRoute from '../routes/index.routes.js';
 import database from '../config/db.config.js';
 import handleError from './error.middleware.js';
+import cors from 'cors';
 
 const middleware = (app) => {
     app.use(express.json());
     app.use(express.urlencoded({ extended: false }))
-    
+    app.use(cors());
     database()
     app.use(indexRoute);
     app.use(handleError);
