bfa3ba9e0ddf206c642ab306e31825e4c524e7c5
Emmanuel <emmanuelugwueze6@gmail.com>
2022-12-31 15:25:56 +0100

    changes

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -10,69 +10,7 @@ import aws from 'aws-sdk';
 
 class AuthController{
          
-    // async uploadImage(req, res) {
-      
-    //     const spacesEndpoint = new aws.Endpoint('nyc3.digitaloceanspaces.com');
-    //     const s3 = new aws.S3({
-         
-           
-            
-    //         endpoint: spacesEndpoint,
-    //         region: 'nyc3',
-           
-         
-    // credentials: {
-    //     accessKeyId: 'DO002YPV6UNB8ZA2WNUK',
-    //     secretAccessKey: 'AOyypUer/Nam9xIdKdtP52YAPeqzFtX63ARaOcimEr4',
-    // }
-    //     });
-        
-            
-    //     s3.upload({
-    //         Bucket: "bnb-spaces", // Add bucket name here
-    //         ACL: "public-read", // Specify whether anyone with link can access the file
-    //         Key: `profileImage/${req.files.image.name}`, // Specify folder and file name
-    //         Body:req.filePath,
-    //         //ContentLength:req.files.image.size,
-    //         ContentType: req.files.image.type,
-           
-            
-    //       }, {
-    //         // partSize: 5,
-    //         // queueSize: 10,
-    //     }, function(err, data){
-    //         if (err) return res.status(500).send({message:`${err}`});
-    //         return res.status(200).send({data:`https://bnb-spaces.nyc3.cdn.digitaloceanspaces.com/${req.files.image.name}`})
-    //        })
-
-    // }
-    // async deleteImage(req, res) {
-    //     const params = {
-    //         Bucket: 'bnb-spaces',
-    //         Key:
-    //             //req.params.imageName
-    //             `profileImage/${req.params.imageName}`,
-    //     };
-      
-    //     const spacesEndpoint = new aws.Endpoint('nyc3.digitaloceanspaces.com');
-    //     const s3 = new aws.S3({
-         
-           
-            
-    //         endpoint: spacesEndpoint,
-    //         region: 'nyc3',
-    // credentials: {
-    //     accessKeyId: 'DO002YPV6UNB8ZA2WNUK',
-    //     secretAccessKey: 'AOyypUer/Nam9xIdKdtP52YAPeqzFtX63ARaOcimEr4',
-    // }
-    //     });
-    //     s3.deleteObject(params, function (error, data) {
-    //         if (error) {
-    //             res.status(500).send({ error: "Something went wrong" });
-    //         }
-    //         console.log("Successfully deleted file", data);
-    //     });
-    // }
+   
    
    
     async createUser(req, res) {
@@ -127,12 +65,7 @@ class AuthController{
         const generatedToken = jwt.sign({ _id: getUser._id, mobile: getUser.mobile }, process.env.TOKEN_SECRET, { expiresIn: '200h' });
         return res.status(200).send({status: true, message: 'user logged in successfully',data:{...omittedData,},token:generatedToken,});
     }
-    async getToken(req,res) {
-        let refreshToken = req.body.refreshToken;
-        if (refreshToken in refreshTokens && this.refreshTokens[refreshToken]== req.body.email) {
-            
-        }
-    }
+   
 }
 
 export default new AuthController();
---
+++ models/report.model.js
@@ -0,0 +1,44 @@
+import mongoose from 'mongoose';
+
+
+const reportSchema = mongoose.Schema({
+    content:{
+        type:String,
+        required: true,
+        
+    },
+    name:{
+        type:String,
+        required: true,
+        
+    },
+    regNo:{
+        type:String,
+        required: true,
+    
+    },
+    phoneNumber:{
+        type:String,
+        required: true,
+    },
+    email:{
+        type:String,
+        required: true,
+    },
+    imageUrl:{
+        type:Array,
+        
+    },
+    voiceNoteUrl: {
+        type:Array,
+    },
+    videoUrl :{
+        type:Array
+    }
+
+},{timestamps:true} );
+
+const reportModel = mongooes
+
+
+
